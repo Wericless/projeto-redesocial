300c9a0fb2e476b9e8a632d193b73f35f5d75844
Wéricles Melo <125150227+Wericless@users.noreply.github.com>
2023-11-16 00:05:50 -0300

    Atualizar o index.js

--- src/API/index.js
+++ src/API/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const rede = axios.create({
-  baseURL: "http://62.72.63.164:5000/ap/",
+  baseURL: "http://62.72.63.164:5000/api/",
 
   headers: {
     "Content-type": "application/json",
